8e017c953b720dc8aaa7f566c37effe1493f6872
sajib9090 <nispap.devil.sajib@gmail.com>
2024-05-03 18:31:38 +0600

    2nd

--- src/app/(pages)/login/page.js
+++ src/app/(pages)/login/page.js
@@ -7,6 +7,7 @@ const LoginPage = () => {
   const [usernameOrEmail, setUsernameOrEmail] = useState("");
   const [password, setPassword] = useState("");
   const [user, setUser] = useState({});
+  const [shop, setShop] = useState({});
   const router = useRouter();
 
   const loginUser = () => {
@@ -16,14 +17,18 @@ const LoginPage = () => {
     };
 
     axios
-      .post("https://shop-management-backend.vercel.app/api/v1/users/auth-user-login", data, {
-        withCredentials: true,
-      })
+      .post(
+        "https://shop-management-backend.vercel.app/api/v1/users/auth-user-login",
+        data,
+        {
+          withCredentials: true,
+        }
+      )
       .then((res) => {
         setUser(res.data.data);
       })
       .catch((error) => {
-        console.log(error.response.data);
+        alert(error.response.data.message);
       });
   };
 
@@ -39,7 +44,9 @@ const LoginPage = () => {
         setUser({});
       })
       .catch((err) => {
-        console.log(err);
+        if (err) {
+          setUser({});
+        }
       });
   };
 
@@ -60,33 +67,67 @@ const LoginPage = () => {
       });
   };
 
+  const refreshToAccess = () => {
+    axios
+      .get(
+        "https://shop-management-backend.vercel.app/api/v1/users/auth-manage-token",
+        {
+          withCredentials: true,
+        }
+      )
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <div>
-      <h1>Login here</h1>
-      <p>{user?.name}</p>
-      <input
-        value={usernameOrEmail}
-        onChange={(e) => setUsernameOrEmail(e.target.value)}
-        type="text"
-        placeholder="username or email"
-      />
-      <br />
-      <br />
-      <input
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        type="password"
-        placeholder="password"
-      />
-      <br />
-      <br />
-      <button onClick={loginUser}>Login</button>
+      {user?.name && (
+        <>
+          <p>user_id: {user?.user_id}</p>
+          <p>name: {user?.name}</p>
+          <p>username: {user?.username}</p>
+          <p>email: {user?.email}</p>
+          <p>mobile: {user?.mobile}</p>
+          <p>address: {user?.address?.detailed_shop_address}</p>
+          <p>country: {user?.address?.country}</p>
+        </>
+      )}
+      {!user?.name && (
+        <>
+          <h1>Login here</h1>
+          <input
+            value={usernameOrEmail}
+            onChange={(e) => setUsernameOrEmail(e.target.value)}
+            type="text"
+            placeholder="username or email"
+          />
+          <br />
+          <br />
+          <input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            placeholder="password"
+          />
+          <br />
+          <br />
+          <button onClick={loginUser}>Login</button>
+        </>
+      )}
+
       <br />
       <br />
       {user?.name && <button onClick={logoutUser}>Logout</button>}
       <br />
       <br />
-      {user?.name && <button onClick={privateRoute}>private route</button>}
+      {user?.name && <button onClick={privateRoute}>admin route</button>}
+      <br />
+      <br />
+      <button onClick={refreshToAccess}>Generate new access token</button>
     </div>
   );
 };
