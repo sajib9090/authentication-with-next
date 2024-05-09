2801b57008ec9e92e0a6b14a13c20dfd24824614
sajib9090 <nispap.devil.sajib@gmail.com>
2024-05-09 19:24:31 +0600

    ssa

--- src/app/baseUrl.js
+++ src/app/baseUrl.js
@@ -1,2 +1,2 @@
-export const baseUrl = "http://localhost:8000";
-// export const baseUrl = "https://shop-management-backend.vercel.app";
+// export const baseUrl = "http://localhost:8000";
+export const baseUrl = "https://shop-management-backend.vercel.app";
